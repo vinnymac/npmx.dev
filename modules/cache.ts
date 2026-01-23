0a085e8a8218a8185b17cc839516e52eac4953b6
Sébastien Chopin <atinux@gmail.com>
2026-01-23 16:33:11 +0100

    feat: add vercel runtime cache for API routes (#4)

---
+++ modules/cache.ts
@@ -0,0 +1,21 @@
+import { defineNuxtModule } from 'nuxt/kit'
+import { provider } from 'std-env'
+
+export default defineNuxtModule({
+  meta: {
+    name: 'vercel-cache',
+  },
+  setup(_, nuxt) {
+    if (provider !== 'vercel') {
+      return
+    }
+
+    nuxt.hook('nitro:config', (nitroConfig) => {
+      nitroConfig.storage = nitroConfig.storage || {}
+      nitroConfig.storage.cache = {
+        driver: 'vercel-runtime-cache',
+        ...nitroConfig.storage.cache,
+      }
+    })
+  },
+})
