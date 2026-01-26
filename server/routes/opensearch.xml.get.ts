5e76d4ccd78189333c63425dcb3a5b5958e76737
Burak Yigit Kaya <ben@byk.im>
2026-01-26 10:35:17 +0000

    feat: add OpenSearch support (#113)

---
+++ server/routes/opensearch.xml.get.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler(async event => {
+  const url = getRequestURL(event)
+  const origin = url.origin
+  return `
+<?xml version="1.0" encoding="UTF-8"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName>npm</ShortName>
+  <Description>Search npm packages on npmx.dev</Description>
+  <InputEncoding>UTF-8</InputEncoding>
+  <Image width="16" height="16" type="image/svg+xml">${origin}/favicon.svg</Image>
+  <Url type="text/html" template="${origin}/search?q={searchTerms}"/>
+  <Url type="application/x-suggestions+json" template="${origin}/api/opensearch/suggestions?q={searchTerms}"/>
+  <Url type="application/opensearchdescription+xml" rel="self" template="${origin}/opensearch.xml"/>
+</OpenSearchDescription>
+  `.trim()
+})
